2d2642c31572c0a2127fc76ce265f38b2241298f
Alexthuita <colealex400@gmail.com>
2022-03-15 03:04:15 -0700

    New add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include"main.h"
+#include "main.h"
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Return:0
+ */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+char c[8] = "_putchar";
+int i = 0;
+while (i < 7)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }

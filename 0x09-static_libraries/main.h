09401251646f12e412e365064d6eb5238bf3896f
Alexthuita <colealex400@gmail.com>
2022-04-01 06:44:51 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,11 +26,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
+char *_strstr(char *haystack, char *needle);>
 
 #endif /* MAIN_H */

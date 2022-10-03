b8f3efcc1362395aa7eca5ccc4f31ff2f290e960
Alexthuita <alexthuita1@gmail.com>
2022-10-03 14:00:06 -0700

    0x1E-search_algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,10 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - searches for a value in an array of integers using
+  * the linear search algorithm
+  * @array: pointer to the first element of the array to search in
+  * @size: number of elements in array
+  * @value: value to search for
+  * Return: index of value or -1 if not found
+  */
